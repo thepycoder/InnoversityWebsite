ec5af3fa3463daa8e39545314ab38fda940e7505
Oka <bono.vanderpoorten@gmail.com>
2016-02-15 17:58:37 +0100

    truth hurts

--- map.php
+++ map.php
@@ -3,7 +3,7 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "inno";
-
+//victor is ne fag
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
